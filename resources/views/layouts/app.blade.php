453cb642a2628fa48815d14729525b5f0fbd7a12
FarisTz <faris.dev.tz@gmail.com>
2026-02-10 08:07:25 +0300

    Authentication issue

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,12 @@
 </head>
 
 <body>
+
+    @php
+         $user = Auth::user();
+    @endphp
+
+
   <div class="loader"></div>
   <div id="app">
     <div class="main-wrapper main-wrapper-1">
@@ -60,7 +66,7 @@
                       application</span>
                     <span class="time">5 Min Ago</span>
                   </span>
-                </a> 
+                </a>
               </div>
               <div class="dropdown-footer text-center">
                 <a href="#">View All <i class="fas fa-chevron-right"></i></a>
@@ -71,8 +77,8 @@
               class="nav-link dropdown-toggle nav-link-lg nav-link-user"> <img alt="image" src="assets/img/user.png"
                 class="user-img-radious-style"> <span class="d-sm-none d-lg-inline-block"></span></a>
             <div class="dropdown-menu dropdown-menu-right pullDown">
-              <div class="dropdown-title">Hello Sarah Smith</div>
-              <a href="profile.html" class="dropdown-item has-icon"> <i class="far
+              <div class="dropdown-title">Hello {{$user->name }}</div>
+              <a href="{{ route('profile.edit') }}" class="dropdown-item has-icon"> <i class="far
 										fa-user"></i> Profile
               </a> <a href="timeline.html" class="dropdown-item has-icon"> <i class="fas fa-bolt"></i>
                 Activities
@@ -80,14 +86,21 @@
                 Settings
               </a>
               <div class="dropdown-divider"></div>
-              <a href="auth-login.html" class="dropdown-item has-icon text-danger"> <i class="fas fa-sign-out-alt"></i>
+              <a href="{{ route('logout') }}" class="dropdown-item has-icon text-danger"> <i class="fas fa-sign-out-alt"></i>
                 Logout
               </a>
             </div>
           </li>
         </ul>
       </nav>
-     @include('admin.partials.side_bar')
+            @if ($user->role === 'admin')
+                @include('admin.partials.side_bar')
+            @elseif($user->role === 'reviewer')
+                @include('reviewer.partials.side_bar')
+            @elseif($user->role === 'user')
+                @include('applicant.partials.side_bar')
+            @endif
+
       <!-- Main Content -->
       <div class="main-content">
         @yield('content')
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,24 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AuthRedirectController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/dashboard', function () {
-    return view('admin.index');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/',[AuthRedirectController::class,'redirect']);
+Route::get('/dashboard', [AuthRedirectController::class,'dashboardRedirect'])->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/logout', [AuthRedirectController::class,'logout'])->middleware(['auth'])->name('logout');
+
+
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
 
 require __DIR__.'/auth.php';

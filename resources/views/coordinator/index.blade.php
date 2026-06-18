88e2d123a71f0245248d5b260ebd0e96e585ffe0
Abromo79 <saidhamad3437@gmail.com>
2026-06-18 10:27:54 +0300

    new coordinator

--- resources/views/coordinator/index.blade.php
+++ resources/views/coordinator/index.blade.php
@@ -1,13 +1,150 @@
 @extends('layouts.app')
 @section('content')
 <section class="section">
-    <div class="container mx-auto">
-        <div class="bg-white p-6 rounded-lg shadow-md" style="color: black">
-            <h1 class="text-2xl font-bold mb-4">Hello, Applicant! 👋</h1>
-            <p class="text-gray-700">
-                Welcome to the KAFAAT Scholarship Application System. Use your dashboard to manage applications, check announcements, and monitor your scholarship status easily.
-            </p>
-        </div>
-    </div>
+          <div class="row ">
+            <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-xs-12">
+              <div class="card">
+                <div class="card-statistic-4">
+                  <div class="align-items-center justify-content-between">
+                    <div class="row ">
+                      <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pr-0 pt-3">
+                        <div class="card-content">
+                          <h5 class="font-15">All Applications</h5>
+                          <h2 class="mt-6 font-20">258</h2>
+
+                        </div>
+                      </div>
+                      <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0">
+                        <div class="banner-img">
+                          <i class="fas fa-folder-open" style="font-size:100px"></i>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-xs-12">
+              <div class="card">
+                <div class="card-statistic-4">
+                  <div class="align-items-center justify-content-between">
+                    <div class="row ">
+                      <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pr-0 pt-3">
+                        <div class="card-content">
+                          <h5 class="font-15"> Pending Application</h5>
+                          <h2 class="mb-3 font-18">1,287</h2>
+
+                        </div>
+                      </div>
+                      <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0">
+                        <div class="banner-img">
+                          <i class="fas fa-clock" style="font-size:100px"></i>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-xs-12">
+              <div class="card">
+                <div class="card-statistic-4">
+                  <div class="align-items-center justify-content-between">
+                    <div class="row ">
+                      <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pr-0 pt-3">
+                        <div class="card-content">
+                          <h5 class="font-15">Approved Application</h5>
+                          <h2 class="mb-3 font-18">128</h2>
+
+                        </div>
+                      </div>
+                      <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0">
+                        <div class="banner-img">
+                          <i class="fas fa-circle-check" style="font-size:100px"></i>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-xs-12">
+              <div class="card">
+                <div class="card-statistic-4">
+                  <div class="align-items-center justify-content-between">
+                    <div class="row ">
+                      <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pr-0 pt-3">
+                        <div class="card-content">
+                          <h5 class="font-15">Rejected</h5>
+                          <h2 class="mb-3 font-18">48,697</h2>
+
+                        </div>
+                      </div>
+                      <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0">
+                        <div class="banner-img">
+                         <i class="fas fa-circle-xmark " style="font-size:100px"></i>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-12 col-sm-12 col-lg-12">
+              <div class="card">
+                  <div class="card-header">
+                    <h4>Recent Application</h4>
+                  </div>
+                  <div class="card-body">
+                    <div class="table-responsive">
+                      <table class="table table-striped" id="table-1">
+                        <thead>
+                          <tr>
+                            <th class="text-center">
+                              #
+                            </th>
+                            <th>Application ID</th>
+                            <th>Student Name</th>
+                            <th>A-Level School</th>
+
+                            <th>Gender</th>
+                            <th>Status</th>
+                            <th>Action</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+
+
+                          <tr>
+                            <td>
+                              12
+                            </td>
+                            <td>APP-ID-NO1</td>
+                            <td class="align-middle">
+
+                                Ali Omar Daudi
+
+                            </td>
+                            <td>
+                              Bukoba Secondary
+                            </td>
+                            <td>Male</td>
+                            <td>
+                              <div class="badge badge-success badge-shadow">Pending</div>
+                            </td>
+                            <td><a href="#" class="btn btn-primary">Detail</a></td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+            </div>
+          </div>
+          <div class="row">
+
+          </div>
 </section>
 @endsection
